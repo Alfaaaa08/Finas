b39632c5d25a8aa12e0e7e2a447fd7827bc7aa90
Théo Lazaris <theo.lazariss@gmail.com>
2024-04-15 20:49:48 -0300

    new file:   templates/base.html new file:   templates/partials/_footer.html new file:   templates/partials/_header.html
    Added a static header and footer. Changed the grid elements on the home page so they are created dinamically using a Django loop

--- home/views.py
+++ home/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home/index.html')
+    
+    grid_elements = [
+        {
+            'a_href'    : 'financial_planning:financial_planning',
+            'div_class' : 'grid-element-container first-element',
+            'i_id'      : 'icon-money', 
+            'i_class'   : 'fa fa-money',
+            'p_id'      : 'title-controle-financeiro'
+        },
+        {
+            'a_href'    : 'financial_planning:financial_planning',
+            'div_class' : 'grid-element-container second-element',
+            'i_id'      : '', 
+            'i_class'   : '',
+            'p_id'      : ''
+        },
+        {
+            'a_href'    : 'financial_planning:financial_planning',
+            'div_class' : 'grid-element-container third-element',
+            'i_id'      : '', 
+            'i_class'   : '',
+            'p_id'      : ''
+        },
+    ]
+   
+    return render(request, 'home/index.html', {'grid_elements': grid_elements})
 
